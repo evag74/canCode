3c19453e7d5b73f7964e3c5f2ca6091dcbdc2f6c
crackalamoo <originalcrackalamoo@gmail.com>
2018-08-18 03:03:54 -0400

    work with image link in top left

--- mobile.js
+++ mobile.js
@@ -1,11 +1,19 @@
 var mobileLimit = 950;
+var iconLinkLimit = 475;
 function mobileFormat(event){
-  var body = document.getElementsByTagName("BODY")[0];
+  var body = document.getElementsByTagName("body")[0];
+  var imageLink = document.getElementsByClassName(".button.left")[0];
   console.log(window.location.pathname);
   if (document.documentElement.clientWidth <= mobileLimit) {
     body.className = "mobile";
+    if (document.documentElement.clientWidth <= iconLinkLimit) {
+      imageLink.style.display = "none";
+    } else {
+      imageLink.style.display = "block";
+    }
   } else {
     body.className = '';
+    imageLink.style.display = "block";
   }
 }
 mobileFormat(null);
