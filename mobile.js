67e5391136341b11c8931e7bb7a4ff72db67e27e
crackalamoo <originalcrackalamoo@gmail.com>
2018-12-02 15:16:45 -0500

    improved menu

--- mobile.js
+++ mobile.js
@@ -1,12 +1,13 @@
 // These are limits for at how many pixels changes to the site happen
-var mobileLimit = 950; // When the screen is this size or smaller, it switches to mobile
-var resourceButtonLimit = 575; // When the screen is this size or smaller, the tabs are replaced with a big "Resources tab"
-var resourceWordLimit = 325; // When the screen is this size or smaller, the "Resources" tab is renamed "More".
+const MOBILE_LIMIT = 950; // When the screen is this size or smaller, it switches to mobile
+const MORE_LIMIT = 575; // When the screen is this size or smaller, the tabs are moved into the "more" tab
 
 var body = document.getElementsByTagName("body")[0];
 var buttons = document.getElementsByClassName("button");
+var expanded = false; // true when the "more" button is clicked
 
-function buttonHighlight(site, button) { // Matches a site to the button in the navbar to highlight when on that site
+function buttonHighlight(site, button) // Matches a site to the button in the navbar to highlight when on that site
+{
   if ((location.pathname.endsWith("/" + site) || location.pathname.endsWith("/" + site + ".html")) && !(location.pathname.endsWith("secret/"))) {
     buttons[button].className = buttons[button].className + " highlight";
   }
@@ -19,54 +20,61 @@ It automatically updates on all pages that are linked to mobile.js.
 This is the main section you need to worry about. */
 document.getElementsByClassName("navbar")[0].innerHTML =
   '<ul>' +
-    '<a href="/" class="button left"><img src="https://canCode.us/Images/canCodePlain1.png"></a>' +
-    '<a href="https://canCode.us/sponsors" class="button left">Sponsors</a>' +
-    '<a href="https://canCode.us/photos" class="button">Photos</a>' +
-    '<a href="https://canCode.us/sites" class="button">Sites</a>' +
-    '<a href="https://canCode.us/team" class="button">Team</a>' +
-    '<a href="https://canCode.us/parent" class="button">Parent</a>' +
-    '<a href="https://canCode.us/student" class="button">Student</a>' +
-    '<a href="https://canCode.us/resources" class="button">Resources</a>' +
-    '<a href="https://canCode.us/contribute" class="button">Contribute</a>' +
-  '</ul>';
+    '<a href="/" class="button left"><img src="https://cancode.us/Images/canCodePlain1.png"></a>' +
+    '<a href="https://www.cancode.us/sponsors" class="button left">Sponsors</a>' +
+    '<a class="button" onclick="expand()">More</a>' +
+    '<a href="https://www.cancode.us/sites" class="button">Sites</a>' +
+    '<a href="https://www.cancode.us/team" class="button">Team</a>' +
+    '<a href="https://www.cancode.us/parent" class="button">Parent</a>' +
+    '<a href="https://www.cancode.us/student" class="button">Student</a>' +
+    '<a href="https://www.cancode.us/contribute" class="button">Contribute</a>' +
+  '</ul>' +
+  '<div id="hiddenNavbar"></div>';
+document.getElementById("hiddenNavbar").innerHTML =
+  '<a href="https://www.cancode.us/photos" class="button">Photos</a>' +
+  '<a href="https://www.cancode.us/studentsafe" class="button">Student Safe</a>';
 
 // Matches sites to navbar buttons that are highlighted
 buttonHighlight("", 0); buttonHighlight("index", 0);
 buttonHighlight("sponsors", 1);
-buttonHighlight("photos", 2);
 buttonHighlight("sites", 3);
 buttonHighlight("team", 4);
 buttonHighlight("parent", 5);
 buttonHighlight("student", 6);
-buttonHighlight("resources", 7);
-buttonHighlight("contribute", 8);
+buttonHighlight("contribute", 7);
 
-function mobileFormat(event){ // Function that is run every time the site is resized to work with the limits explained above
+function expand() {
+  if (expanded) {
+    expanded = false;
+    buttons[2].innerHTML = "More";
+    document.getElementById("hiddenNavbar").style.display = "none";
+  } else {
+    expanded = true;
+    buttons[2].innerHTML = "Hide";
+    document.getElementById("hiddenNavbar").style.display = "flex";
+  }
+}
+
+function mobileFormat(event) // Function that is run every time the site is resized to work with the limits explained above
+{
   console.log(window.location.pathname);
-  if (document.documentElement.clientWidth <= mobileLimit) {
+  if (document.documentElement.clientWidth <= MOBILE_LIMIT) {
     body.className = "mobile";
   } else {
     body.className = '';
   }
-  if (document.documentElement.clientWidth <= resourceButtonLimit) {
-    buttons[2].style.display = "none";
-    buttons[3].style.display = "none";
+  if (document.documentElement.clientWidth <= MORE_LIMIT) {
+    buttons[2].style.display = "block";
     buttons[4].style.display = "none";
     buttons[5].style.display = "none";
     buttons[6].style.display = "none";
-    buttons[7].style.display = "block";
+    buttons[7].style.display = "none";
   } else {
     buttons[2].style.display = "block";
-    buttons[3].style.display = "block";
     buttons[4].style.display = "block";
     buttons[5].style.display = "block";
     buttons[6].style.display = "block";
-    buttons[7].style.display = "none";
-  }
-  if (document.documentElement.clientWidth <= resourceWordLimit) {
-    buttons[7].innerText = "More";
-  } else {
-    buttons[7].innerText = "Resources";
+    buttons[7].style.display = "block";
   }
 }
 var i = 1;
