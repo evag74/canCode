eccc1a5de893c241a59f33c3b09d7ab8cf877420
crackalamoo <originalcrackalamoo@gmail.com>
2019-01-05 18:57:44 -0500

    winter camp update

--- footer.js
+++ footer.js
@@ -2,16 +2,17 @@
 var footerHTML = '<div class="divTable">' +
 
 '<div class="divTableCell">' + '<img src="https://cancode.us/Images/canCodePlain2.png" width="30%" align="left">' + '<br>' + '<br>' + '<br>' +
-'<a href="https://cancode.us"><p style="line-height:20%" align="left">Home<p></a>' +
-'<a href="https://cancode.us/sponsors"><p style="line-height:20%" align="left">Sponsors<p></a>' +
-'<a href="https://cancode.us/contribute"><p style="line-height:20%" align="left">Contribute<p></a>' +
-'<a href="https://cancode.us/student"><p style="line-height:20%" align="left">Student Resources<p></a>' +
-'<a href="https://cancode.us/parent"><p style="line-height:20%" align="left">Parent Resources<p></a>' +
-'<a href="https://cancode.us/team"><p style="line-height:20%" align="left">Meet our Team<p></a>' +
-'<a href="https://cancode.us/sites"><p style="line-height:20%" align="left">Participating Sites<p></a>' +
-'<a href="https://cancode.us/photos"><p style="line-height:20%" align="left">Photos<p></a>' +
-'<a href="https://cancode.us/studentsafe"><p style="line-height:20%" align="left">Student Safe App<p></a>' +
-'<a href="https://cancode.us/secret"><p style="line-height:20%" align="left">Teacher Portal<p></a></div>' +
+'<a href="https://www.cancode.us"><p style="line-height:20%" align="left">Home<p></a>' +
+'<a href="https://www.cancode.us/sponsors"><p style="line-height:20%" align="left">Sponsors<p></a>' +
+'<a href="https://www.cancode.us/contribute"><p style="line-height:20%" align="left">Contribute<p></a>' +
+'<a href="https://www.cancode.us/student"><p style="line-height:20%" align="left">Student Resources<p></a>' +
+'<a href="https://www.cancode.us/parent"><p style="line-height:20%" align="left">Parent Resources<p></a>' +
+'<a href="https://www.cancode.us/team"><p style="line-height:20%" align="left">Meet our Team<p></a>' +
+'<a href="https://www.cancode.us/sites"><p style="line-height:20%" align="left">Participating Sites<p></a>' +
+'<a href="https://www.cancode.us/photos"><p style="line-height:20%" align="left">Photos<p></a>' +
+'<a href="https://www.cancode.us/games"><p style="line-height:20%" align="left">Games<p></a>' +
+'<a href="https://www.cancode.us/studentsafe"><p style="line-height:20%" align="left">Student Safe App<p></a>' +
+'<a href="https://www.cancode.us/secret"><p style="line-height:20%" align="left">Teacher Portal<p></a></div>' +
 
 '<div class="divTableCell">' +
 '<br><a href="https://www.scratch.mit.edu" target="_blank"><img src="https://cancode.us/Images/Scratch_cat.png" width="40%"></a></div>' +
