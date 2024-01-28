d960cb98ba311b5bff3271fa92c5ded212aac7d8
JobaHD <92954275+JobaHD@users.noreply.github.com>
2024-01-27 21:09:09 -0500

    feat: organized files; making each js module its own file. Now working on bug where json is not there after reload, likely some quirk of async stuff.

---
+++ testSite/progressBars.js
@@ -0,0 +1,16 @@
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('progress-bar-animation');
+            return;
+        }
+
+        entry.target.classList.remove('progress-bar-animation');
+    })
+});
+
+window.addEventListener('load', function() {
+    const progressBars = document.querySelectorAll(".progress-bar-inside");
+
+    progressBars.forEach(bar => observer.observe(bar));
+})
--- testSite/teamCarousel.js
+++ testSite/teamCarousel.js
@@ -1,4 +1,11 @@
-import { TeamMember } from './membersParse.js'
+//for slide information
+class TeamMember {
+    constructor(highSchool, name, description) {
+        this.highSchool = highSchool;
+        this.name = name;
+        this.description = description;
+    }
+}
 
 // Initialize TeamMember Arr from JSON file.
 const data = await fetch('./teamData.json')
@@ -6,24 +13,10 @@ const data = await fetch('./teamData.json')
 
 const teamArr = data.map(obj => new TeamMember(obj.HighSchool, obj.Name, obj.Description));
 
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('progress-bar-animation');
-            return;
-        }
-
-        entry.target.classList.remove('progress-bar-animation');
-    })
-});
 
 // All startup functionality goes into this function.
 // Triggered when page loads
 window.addEventListener('load', function() {
-    const progressBars = document.querySelectorAll(".progress-bar-inside");
-
-    progressBars.forEach(bar => observer.observe(bar));
-
     const teamMemberBanners = document.querySelectorAll(".members-card-div");
 
     console.log(teamMemberBanners);
