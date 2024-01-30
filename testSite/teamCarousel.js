32089417476b2eb90d21b2a31c299073e05a1b55
JobaHD <92954275+JobaHD@users.noreply.github.com>
2024-01-29 21:12:47 -0500

    feat: working on sliding window for infinite carousel.

--- testSite/teamCarousel.js
+++ testSite/teamCarousel.js
@@ -17,16 +17,26 @@ const teamArr = data.map(obj => new TeamMember(obj.HighSchool, obj.Name, obj.Des
 // All startup functionality goes into this function.
 // Triggered when page loads
 window.addEventListener('load', function() {
+    let bannerStart = 0;
+    let bannerEnd = bannerStart + 4;
     const teamMemberBanners = document.querySelectorAll(".members-card-div");
+    const bannerLength = teamMemberBanners.length;
 
     console.log(teamMemberBanners);
     console.log(teamArr);
 
-    teamMemberBanners.forEach((banner, i) => {
-        banner.children[1].innerHTML = teamArr[i].highSchool;
-        banner.children[2].innerHTML = teamArr[i].name;
-        banner.children[3].innerHTML = teamArr[i].description;
-    });
+    // length: 5, i = 3, endI = 7,
+    teamMemberBanners
+        .slice(bannerStart, bannerEnd < bannerLength ? bannerEnd : bannerLength - 1)
+        .concat(
+            // working on this part rn, array should have a sliding window of size 4
+            // that wraps around if it goes past the last el.
+            teamMemberBanners
+                .slice(0, bannerEnd < bannerLength ? 0 : (bannerEnd - bannerLength) + 1)
+        )
+        .forEach((banner, i) => {
+            banner.children[1].innerHTML = teamArr[i].highSchool;
+            banner.children[2].innerHTML = teamArr[i].name;
+            banner.children[3].innerHTML = teamArr[i].description;
+        });
 })
-
-
