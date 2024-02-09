966bd93e25fa69666344d853577ff4ff8bc3a9ef
JobaHD <92954275+JobaHD@users.noreply.github.com>
2024-02-08 20:59:21 -0500

    feat: fixed img-content-layout component, added alternate layout.

--- testSite/Components/layouts/imgContentLayout.js
+++ testSite/Components/layouts/imgContentLayout.js
@@ -1,48 +1,46 @@
 class ImageContentLayout extends HTMLElement {
-    static observedAttributes = ["image1", "image2", "section", "title", "description", "buttonContent"];
+    static observedAttributes = ["image1", "image2", "section", "title", "description", "buttonContent", "altLayout"];
 
     connectedCallback() {
-        this.innerHTML = `
-    <section class="img-content-layout">
-        <div class="aboutUs-left">
-            <div class="image-container">
-                <img src="Images/students1.jpg" alt="" class="base-image">
-                <img src="Images/students2.jpg" class="top-image">
-            </div>
-        </div>
-
-        <div class="text-block">
-            <h4>About Us</h4>
-            <hr>
-            <h2>Your support is really poweful</h2>
-            <p>canCode is a group of high school students in Palm Beach County, Florida, committed to teaching
-                elementary
-                school students computer programming through engaging workshops.</p>
-            <button class="button-solid">Read more</button>
-        </div>
-    </section>
-            `
-    }
-
-    attributeChangedCallback(image1, image2, section, title, description, buttonContent) {
-        this.innerHTML = `
+        if (!this.getAttribute("altLayout")) {
+            this.innerHTML = `
     <section class="img-content-layout">
         <div class="">
             <div class="image-container">
-                <img src="${image1}" alt="" class="base-image">
-                <img src="${image2}" class="top-image">
+                <img src="${this.getAttribute("image1")}" alt="" class="base-image">
+                <img src="${this.getAttribute("image2")}" class="top-image">
             </div>
         </div>
 
         <div class="text-block">
-            <h4>${section}</h4>
+            <h4>${this.getAttribute("section")}</h4>
             <hr>
-            <h2>${title}</h2>
-            <p>${description}</p>
-            <button class="button-solid">${buttonContent}</button>
+            <h2>${this.getAttribute("title")}</h2>
+            <p>${this.getAttribute("description")}</p>
+            <button class="button-solid">${this.getAttribute("buttonContent")}</button>
         </div>
     </section>
             `
+        } else {
+            this.innerHTML = `
+    <section class="img-content-layout">
+
+        <div class="text-block">
+            <h4>${this.getAttribute("section")}</h4>
+            <hr>
+            <h2>${this.getAttribute("title")}</h2>
+            <p>${this.getAttribute("description")}</p>
+            <button class="button-solid">${this.getAttribute("buttonContent")}</button>
+        </div>
+        <div class="">
+            <div class="image-container">
+            <img src="${this.getAttribute("image1")}" alt="" class="base-image">
+            <img src="${this.getAttribute("image2")}" class="top-image">
+            </div>
+        </div>
+    </section>
+                `;
+        }
     }
 
 }
