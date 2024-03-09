750fcb626009077a0ab94e2e60ecaa7553b34dab
JobaHD <92954275+JobaHD@users.noreply.github.com>
2024-03-09 08:13:15 -0500

    feat: now runningNumbers are working correctly, with custom data attributes, they might be off by one of two but it should't matter too much.

--- Components/contributionContent/contributionContent.js
+++ Components/contributionContent/contributionContent.js
@@ -1,6 +1,6 @@
 export class ContributionContent extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = `
+  connectedCallback() {
+    this.innerHTML = `
             <div class="contribution-content">
                 <div class="contribution-img-container">
                     <img class="contribution-img" src="../../Images/eva.jpg" alt="image of eva">
@@ -11,20 +11,19 @@ export class ContributionContent extends HTMLElement {
                 </p>
             </div>
             `;
-    }
+  }
 
-    static createContributionContent(contributionContainer, contributionData) {
-        const newContrib = document.createElement("my-contribution-content");
-        const { Name, Quote, Picture } = contributionData;
-        contributionContainer.appendChild(newContrib);
+  static createContributionContent(contributionContainer, contributionData) {
+    const newContrib = document.createElement("my-contribution-content");
+    const { Name, Quote, Picture } = contributionData;
+    contributionContainer.appendChild(newContrib);
 
-        newContrib.querySelector("h3").innerText = `${Name}`;
-        newContrib.querySelector("p").innerText = `${Quote}`;
-        newContrib.querySelector("img").src = `${Picture}`;
+    newContrib.querySelector("h3").innerText = `${Name}`;
+    newContrib.querySelector("p").innerText = `${Quote}`;
+    newContrib.querySelector("img").src = `${Picture}`;
 
-        return newContrib;
-    }
-
+    return newContrib;
+  }
 }
 
 customElements.define("my-contribution-content", ContributionContent);
--- runningNumbers.js
+++ runningNumbers.js
@@ -1,31 +1,31 @@
-(function() {
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                countUp(entry.target, 1000);
-                return;
-            }
+(function () {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        countUp(entry.target, entry.target.getAttribute("data-value"));
+        return;
+      }
 
-            entry.target.innerHTML = 0;
-        })
+      entry.target.innerHTML = 0;
     });
+  });
 
-    function countUp(element, limit) {
-        const increment = Math.floor(limit / 1000);
-        let value = 0;
+  function countUp(element, limit) {
+    const increment = Math.floor(limit / 1000);
+    let value = 0;
 
-        const interval = setInterval(() => {
-            value += increment;
-            if ((element.innerHTML = value) >= limit) {
-                element.innerHTML += "+";
-                clearInterval(interval);
-            }
-        }, 1);
-    }
+    const interval = setInterval(() => {
+      value += increment;
+      if ((element.innerHTML = value) >= limit) {
+        element.innerHTML += "+";
+        clearInterval(interval);
+      }
+    }, 1);
+  }
 
-    window.addEventListener("load", function() {
-        const runningNumbers = document.querySelectorAll(".running-numbers");
+  window.addEventListener("load", function () {
+    const runningNumbers = document.querySelectorAll(".running-numbers");
 
-        runningNumbers.forEach(num => observer.observe(num));
-    });
+    runningNumbers.forEach((num) => observer.observe(num));
+  });
 })();
